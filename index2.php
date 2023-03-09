8441dd7bb53967ba4d4cc7ffe60c936e2f3a18d6
JulianDak <118720788+JulianDak@users.noreply.github.com>
2023-03-09 08:24:53 +0100

    Kursarbeit2

---
+++ index2.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+	<h1>Kursarbeit2SoSe2023</h1>
+	Herzlich Wilkommen Lisa Obst
+</body>
+</html>
